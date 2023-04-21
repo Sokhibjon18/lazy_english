5ca908a77a7e32f2f09e9f4c4ec7fb5a390e3f5e
Sokhibjon18 <safarovsoxibjon18@gmail.com>
2023-04-21 10:17:24 +0500

    leaderboard but not as you said

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lazy_english/features/leaderboard/pages/leaderboard_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: LeaderboardPage(),
     );
   }
 }
